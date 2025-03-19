4772c0b790993823f1ca83a49a7e2a6857d4e2af
wuyu0107 <mkim050107@gmail.com>
2025-03-19 21:15:47 +0700

    (4) Simulation of slow request

--- README.md
+++ README.md
@@ -21,3 +21,14 @@ Now, after reading the incoming request using a buffered reader, it defines a HT
 ![commits 3 screen](/assets/images/commit3-1.png)
 
 Now, the ```handle_connection``` function can handle 404 response. Instead of reading all the lines, the function now reads the first line of the HTTP request (e.g., ```GET / HTTP/1.1```). If request matches the request ```GET / HTTP/1.1```, it will serve the page ```hello.html``` with a 200 OK status. Otherwise, it will serve the page ```404_bad.html``` with a 404 Not Found status.  
+
+### Commit 4 Reflection Notes
+
+First, the new code check with the request:
+- If the request is for ```/```, the server responds with ```hello_html``` immediately
+- If the request is for ```/sleep```, the server pauses for 10 seconds before responds
+- Otherwise, it returns a 404 error, responding with ```404_bad.html```. 
+
+If we open window 1(```127.0.0.1/sleep```) first then window 2(```127.0.0.1/```), we can see that the server pauses for 10 seconds before sending a response. We can also see that window 2 also delays its reponse instead of loading immediately. 
+
+This happens of the server being a single-threaded, meaning it can only handle one request at a time. The ```TcpListener::incoming()``` loop processes one connection at a time and when a request to ```/sleep``` comes int, the server executes the ```thread::sleep(Duration::from_secs(10))```, which will stop the entire server for 10 seconds, not allowing any other requests to be handled. 
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 
@@ -19,25 +21,21 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(10));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
 
-        let response =
-            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404_bad.html").unwrap();
-        let length = contents.len();
-
-        let response =
-            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
 
 
